5fa3782eb34bb9bb424dd1a72fd665eb708dd8e9
Cory Smith <cory.smith1522@outlook.com>
2020-11-30 19:39:36 -0500

    modify seed data

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_30_204409) do
+ActiveRecord::Schema.define(version: 2020_11_30_231207) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,11 @@ ActiveRecord::Schema.define(version: 2020_11_30_204409) do
     t.integer "price"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "meals_orders", id: false, force: :cascade do |t|
+    t.bigint "order_id", null: false
+    t.bigint "meal_id", null: false
   end
 
   create_table "orders", force: :cascade do |t|
