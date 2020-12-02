33f825dc7804a4b71580a23c905c96424931b84e
Cory Smith <cory.smith1522@outlook.com>
2020-12-02 16:55:13 -0500

    start on create order

---
+++ client/src/screens/CreateOrder.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+
+export default function CreateOrder() {
+  const [formData, setFormData] = useState({
+    name_on_order: ''
+  })
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData(prevState => ({
+      ...prevState,
+      [name]: value
+    }))
+  }
+  return (
+    <form>
+      <h2>Your New Order</h2>
+      <label htmlFor='name_on_order'>Name</label>
+      <input
+        type='text'
+        name='name_on_order'
+        value={formData.name_on_order}
+        onChange={handleChange}
+      />
+      <button>Submit</button>
+    </form>
+  )
+}
